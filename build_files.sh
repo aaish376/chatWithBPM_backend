9693067efb015853cae4deff6fd5f72cd47d96b9
aaish376 <bcsf21m.pucit@gmail.com>
2025-03-24 21:03:41 +0500

    for vercel

--- build_files.sh
+++ build_files.sh
@@ -1,27 +1,4 @@
-{ 
-"version": 2, 
-"builds": [ 
-{ 
-"src": "ChatWithBPMBackend/wsgi.py", 
-"use": "@vercel/python", 
-You, 57 minutes ago added vercel 
-"config": { "maxLambdaSize": "15mb", "runtime": "python3.9" } 
-}, 
-{ 
-"src": "build_files.sh", 
-"use": "@vercel/static-build", 
-"config": { 
-"distDir": "staticfiles_build" 
-} 
-
-], 
-"routes":[ 
-{ 
-"src": "/static/(.*)", 
-"dest": "/static/$1" 
-}, 
-{ 
-"src": "/(.*)", 
-"dest": "ChatWithBPMBackend/wsgi.py" 
-} 
-}
+echo "BUILD START"
+python3.11.0 -m pip install -r requirements.txt
+python3.11.0 manage.py collectstatic --noinput --clear
+echo "BUILD END"
