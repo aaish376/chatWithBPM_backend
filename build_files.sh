a17c73446a1c711fa3a12bfd9cdf678e8afddd98
aaish376 <bcsf21m.pucit@gmail.com>
2025-03-24 21:19:06 +0500

    for vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 echo "BUILD START"
-python3.11.0 -m pip install -r requirements.txt
-python3.11.0 manage.py collectstatic --noinput --clear
+python3.11 -m pip install -r requirements.txt
+
+python3.11 manage.py makemigrations --noinput
+python3.11 manage.py migrate --noinput
+python3.11 manage.py collectstatic --noinput --clear
 echo "BUILD END"
