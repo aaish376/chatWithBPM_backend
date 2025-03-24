498aff64975ec7ff1f134945dcea8a594e5b23e1
aaish376 <bcsf21m.pucit@gmail.com>
2025-03-24 22:35:09 +0500

    base

--- ChatWithBPMBackend/settings.py
+++ ChatWithBPMBackend/settings.py
@@ -10,10 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+from pathlib import Path
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -155,15 +162,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#for vercel
-import os
-STATICFILES_DIRS = [BASE_DIR / "static"]  # Correct ✅
-
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5180",
+    "https://chatwithbpm.netlify.app/",
 ]
 
 from datetime import timedelta
