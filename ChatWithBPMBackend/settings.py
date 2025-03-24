48354cde23a4a7dd8767459aaeedb206572f8932
aaish376 <bcsf21m.pucit@gmail.com>
2025-03-24 20:59:30 +0500

    for vercel

--- ChatWithBPMBackend/settings.py
+++ ChatWithBPMBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wve4sa)-a07xf(*vv%ei^z)*jvzs3x381jzy=!z1&*t8!=%=n1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -153,6 +153,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#for vercel
+import os
+STATICFILES_DIRS= os.path.join(BASE_DIR, 'static')
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5180",
