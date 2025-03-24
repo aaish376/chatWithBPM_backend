da775a799b1c3ce9afe877991d539b28117b90a9
aaish376 <bcsf21m.pucit@gmail.com>
2025-03-24 23:46:50 +0500

    db

--- ChatWithBPMBackend/settings.py
+++ ChatWithBPMBackend/settings.py
@@ -18,8 +18,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 
 
