1adcf7c085a762e46fb2d83a193f6484ffe0b626
aaish376 <bcsf21m.pucit@gmail.com>
2025-03-24 21:55:37 +0500

    ver change

--- rag/urls.py
+++ rag/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import bpmn_to_nl_view, query_view
 
 urlpatterns = [
-    path("bpmn-to-nl/", bpmn_to_nl_view, name="bpmn_to_nl"),
-    path("query/", query_view, name="query"),
+    # path("bpmn-to-nl/", bpmn_to_nl_view, name="bpmn_to_nl"),
+    # path("query/", query_view, name="query"),
 ]
